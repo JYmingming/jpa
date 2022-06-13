e23fec0e03c3c601f6b67850a9885821aa0e7102
JYmingming <sla5962@gmail.com>
2022-06-13 21:10:36 +0900

    project update

---
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -0,0 +1,15 @@
+package hellojpa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class JpaMain {
+    public static void main(String[] args) {
+        EntityManagerFactory emf =Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+        //code
+        em.close();
+        emf.close();
+    }
+}
