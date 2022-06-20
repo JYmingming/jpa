e2d425f6cf2675d161182baa52d4cfbfdf072ece
JYmingming <sla5962@gmail.com>
2022-06-20 21:00:21 +0900

    project update

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -2,14 +2,34 @@ package hellojpa;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class JpaMain {
     public static void main(String[] args) {
         EntityManagerFactory emf =Persistence.createEntityManagerFactory("hello");
+
+
         EntityManager em = emf.createEntityManager();
-        //code
-        em.close();
-        emf.close();
+        EntityTransaction tx= em.getTransaction();
+
+        tx.begin(); //Transaction start
+        try{
+//            Member findMember = em.find(Member.class,1L);
+            Member member=em.find(Member.class,150L);
+
+            member.setName("hhh");
+
+            em.persist(member);
+
+            tx.commit(); //Transaction commit
+        } catch (Exception e){
+            tx.rollback();
+        }finally {
+            em.close();
+        }
+
+        emf.close(); //EntityManagerFactory close
     }
 }
