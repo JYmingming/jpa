e6bf8e542e3d0adc3f8a39879fa6e0e07b84c5bf
JYmingming <sla5962@gmail.com>
2022-06-27 21:25:40 +0900

    jpa example2 update

---
+++ jpashop/src/main/java/jpabook/jpashop/domain/Delivery.java
@@ -0,0 +1,21 @@
+package jpabook.jpashop.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Delivery {
+    @Id @GeneratedValue
+    private Long id;
+
+    private String name;
+    private String city;
+    private String street;
+    private DeliveryStatus status;
+
+
+
+
+
+}
