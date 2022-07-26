308033c43587131bb9632741d52855d5faed8b7d
JYmingming <sla5962@gmail.com>
2022-07-26 20:48:10 +0900

    jpa example update

--- jpashop/src/main/java/jpabook/jpashop/JpaMain.java
+++ jpashop/src/main/java/jpabook/jpashop/JpaMain.java
@@ -25,10 +25,7 @@ public class JpaMain {
             Book book=new Book();
             book.setName("JPA");
             book.setAuthor("김영한");
-
             em.persist(book);
-
-
 
             tx.commit(); //Transaction commit
         } catch (Exception e){
